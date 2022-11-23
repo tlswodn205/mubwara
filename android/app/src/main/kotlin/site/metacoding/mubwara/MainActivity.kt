a0a340599bda69c5d49fcc408542cc4e5d088f73
tlswodn205 <tlswodn205@naver.com>
2022-11-23 14:20:53 +0900

    기본세팅완료

---
+++ android/app/src/main/kotlin/site/metacoding/mubwara/MainActivity.kt
@@ -0,0 +1,6 @@
+package site.metacoding.mubwara
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          body: Container(
+        child: Center(child: Text("메인")),
+      )),
+    );
+  }
+}
