963fd951d2c0e527e5f39e56d6d1af060451cc39
donghun-park-code <78709260+donghun-park-code@users.noreply.github.com>
2022-12-01 10:57:20 +0900

    ㅇㅇ (#7)
    Co-authored-by: sopu55544@gmail.com <jhnadas@naver.com>
    Co-authored-by: tlswodn205 <80434390+tlswodn205@users.noreply.github.com>

--- lib/views/layout/default_layout.dart
+++ lib/views/layout/default_layout.dart
@@ -1,3 +1,4 @@
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 
 class DefaultLayout extends StatelessWidget {
@@ -24,16 +25,83 @@ class DefaultLayout extends StatelessWidget {
       body: child,
       bottomNavigationBar: bottomNavigationBar,
       floatingActionButton: floatingActionButton,
+      endDrawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.home,
+              ),
+              title: const Text('Page 1'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.train,
+              ),
+              title: const Text('Page 2'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 
-  AppBar? renderAppBar(){
-    if(title == null){
+  AppBar? renderAppBar() {
+    if (title == null) {
       return null;
-    }else{
+    } else {
       return AppBar(
+        toolbarHeight: 80,
+        centerTitle: true,
         backgroundColor: Colors.white,
         elevation: 0,
+        actions: [
+          Stack(
+            children: [
+              Container(
+                padding: EdgeInsets.only(
+                  top: 25,
+                  right: 5
+                ),
+                child: Icon(
+                  Icons.notifications,
+                  size: 30,
+                ),
+              ),
+              Positioned(
+                top: 30,
+                right: -1,
+                child: Badge(
+                  shape: BadgeShape.circle,
+                  badgeColor: Colors.red,
+                  borderRadius: BorderRadius.circular(
+                    5,
+                  ),
+                  badgeContent: Text(
+                    '2',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
         title: Text(
           title!,
           style: TextStyle(
