40a3b73ab4bcee8be823bec8d9e01b2146d3d345
tlswodn205 <tlswodn205@naver.com>
2022-12-13 12:40:51 +0900

    체크박스 체크 완료

--- lib/views/page/reservation_management_page/component/checkbox/checkboxlist.dart
+++ lib/views/page/reservation_management_page/component/checkbox/checkboxlist.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 class CheckBoxListTileModel {
-  int userId;
+  int optionId;
   String img;
   String title;
   bool isCheck;
 
   CheckBoxListTileModel(
-      {required this.userId,
+      {required this.optionId,
       required this.img,
       required this.title,
       required this.isCheck});
@@ -15,42 +15,42 @@ class CheckBoxListTileModel {
   static List<CheckBoxListTileModel> getUsers() {
     return <CheckBoxListTileModel>[
       CheckBoxListTileModel(
-          userId: 1,
+          optionId: 1,
           img: 'assets/images/feature/24_free.png',
           title: "24시간",
           isCheck: false),
       CheckBoxListTileModel(
-          userId: 2,
+          optionId: 2,
           img: 'assets/images/feature/disabled.png',
           title: "장애인석",
           isCheck: false),
       CheckBoxListTileModel(
-          userId: 3,
+          optionId: 3,
           img: 'assets/images/feature/dog.png',
           title: "반려동물 가능",
           isCheck: false),
       CheckBoxListTileModel(
-          userId: 4,
+          optionId: 4,
           img: 'assets/images/feature/drink.png',
           title: "콜키지",
           isCheck: false),
       CheckBoxListTileModel(
-          userId: 5,
+          optionId: 5,
           img: 'assets/images/feature/nokids.png',
           title: "노키즈존",
           isCheck: false),
       CheckBoxListTileModel(
-          userId: 6,
+          optionId: 6,
           img: 'assets/images/feature/park.png',
           title: "발렛파킹",
           isCheck: false),
       CheckBoxListTileModel(
-          userId: 7,
+          optionId: 7,
           img: 'assets/images/feature/rettering.png',
           title: "레터링",
           isCheck: false),
       CheckBoxListTileModel(
-          userId: 8,
+          optionId: 8,
           img: 'assets/images/feature/smoke.png',
           title: "흡연석",
           isCheck: false),
